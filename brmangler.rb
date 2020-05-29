ba5676f7e6095ab30d0817be324166bf3a8db9c4
L0stControl <35183856+L0stControl@users.noreply.github.com>
2020-05-29 19:54:45 -0300

    Update brmangler.rb

--- brmangler.rb
+++ brmangler.rb
@@ -19,7 +19,7 @@ logo = '
          Brazilian wordlist generator hu3hu3hu3                                                                           
 '.red
 
-options = {:file => nil, :min => 1, :max => 20, :leet => true, :insane => false, :cap => true, :upcase => true, :date => true, :special => true, :two => false, :qt => false}
+options = {:file => nil, :min => 1, :max => 20, :leet => true, :insane => false, :cap => true, :upcase => true, :date => true, :special => true, :months => true, :two => false}
 
 parser = OptionParser.new do|opts|
     opts.banner = "\n Usage: ./brmangler.rb [options]\n\n"
@@ -44,16 +44,16 @@ parser = OptionParser.new do|opts|
         options[:cap] = false
     end
 
-    opts.on('-q', '--qt', 'Calculate the quantity of outputs') do |qt|
-        options[:qt] = true
-    end
-
     opts.on('-u', '--upcase', 'DISABLE uppercase') do |upcase|
         options[:upcase] = false
     end
 
     opts.on('-d', '--date', 'DISABLE dates (e.g. "password@020212")') do |date|
         options[:date] = false
+    end
+ 
+    opts.on('-j', '--months', 'DISABLE months (e.g. "janeiro@020212")') do |date|
+      options[:months] = false
     end
 
     opts.on('-s', '--special', 'DISABLE passwords with special characters') do |special|
@@ -73,162 +73,173 @@ end
 parser.parse!
 
 if options[:file] == nil
-
     print logo
     puts "#{parser}\n"
     exit
-
 else 
-
     begin 
-
-    inputFile = File.open(options[:file], "r")
-    inputData = inputFile.read
-    inputFile.close 
-
+      inputFile = File.open(options[:file], "r")
+      inputData = inputFile.read
+      inputFile.close 
     rescue 
-
-    puts "[Error] File #{options[:file]} not found".red
-    exit
-
+      puts "[Error] File #{options[:file]} not found".red
+      exit
     end
-
 end
 
 # Variables and Constants
 
-SPECIAL = ['!','@','#','$','%','&','*']
+SPECIAL = ['!','@','#','$','%','&','*', '?']
 REPLACEMENTS1 = [["a", "4"], ["e", "3"], ["s", "$"], ["o", "0"], ["i", "1"]]
 REPLACEMENTS2 = [["a", "@"], ["e", "&"], ["s", "5"], ["o", "0"], ["t", "7"]]
+MONTHS = ['janeiro','fevereiro','março','abril','maio','junho','julho','agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
 $min = options[:min]
 $max = options[:max]
 $date = options[:date]
 $special = options[:special]
 $insane = options[:insane]
 $upcase = options[:upcase]
-$qt = options[:qt]
 $leet = options[:leet]
 $capitalize = options[:cap]
 $file = options[:file]
 $two = options[:two]
+$months = options[:months]
 
 # Functions
 
-def checkPasswdSize(password)
-
-  if password.length >= $min && password.length <= $max
-
-    puts password.chomp
-
-  end
-
-end
-
-
 def wordSpecial(word)
-  
-  if $special == true
 
+  if $special == true && word.length + 1 >= $min && word.length + 1 <= $max
     (SPECIAL.length).times do |times|
-
-      checkPasswdSize(word.chomp + SPECIAL[times])
-      checkPasswdSize(SPECIAL[times] + word.chomp)
-      
-      (SPECIAL.length).times do |times2|
-        
-        checkPasswdSize(SPECIAL[times] + word.chomp + SPECIAL[times2])
-     
-      end
-
+      puts word + SPECIAL[times]
+      puts SPECIAL[times] + word
     end
-
   end
-
+  if $special == true && word.length + 2 >= $min && word.length + 1 <= $max
+    (SPECIAL.length).times do |times|
+        (SPECIAL.length).times do |times2|
+          puts SPECIAL[times2] + word + SPECIAL[times2]
+        end
+    end
+  end
 end
 
 def wordSpecialNum(word)
 
-    (SPECIAL.length).times do |times|  
-
-      (0..9999).each do |num|
-
-        checkPasswdSize("#{num}#{word.chomp}#{SPECIAL[times]}")
-        checkPasswdSize("#{SPECIAL[times]}#{word.chomp}#{num}")
-        checkPasswdSize("#{word.chomp}#{SPECIAL[times]}#{num}")      
-
+  if word.length + 3 <= $max
+    (SPECIAL.length).times do |times|
+      (0..99).each do |num|
+        if word.length + num.to_s.length + 1 >= $min
+        puts "#{num}#{word}#{SPECIAL[times]}"
+        puts "#{SPECIAL[times]}#{word}#{num}"
+        puts "#{word}#{SPECIAL[times]}#{num}"
+        else
+          next
+        end
       end
-
     end
+  end
 
+  if word.length + 4 <= $max
+    (SPECIAL.length).times do |times|
+      (100..9999).each do |num|
+        if word.length + num.to_s.length + 1 >= $min
+          puts "#{num}#{word}#{SPECIAL[times]}"
+          puts "#{SPECIAL[times]}#{word}#{num}"
+          puts "#{word}#{SPECIAL[times]}#{num}"
+        else
+          next
+        end
+      end
+    end
+  end
 end
 
 def wordNum(word)
-  
-  (0..9999).each do |num|
-
-    checkPasswdSize("#{num}#{word.chomp}")
-    checkPasswdSize("#{word.chomp}#{num}") 
-
+  if word.length + 1 >= $min
+    (0..9999).each do |num|
+      if word.length + num.to_s.length > $max
+        break
+      else
+      puts "#{num}#{word}"
+      puts "#{word}#{num}"
+      end
+    end
   end
-
 end
 
 
-def wordDate(word) #
+def wordDate(word)
 
-  if $date == true
+  if $date == true && word.length + 6 >= $min && word.length + 6 <= $max
   
-    rangeDate = ((70.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 70 years 
+    rangeDate = ((60.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 60 years 
       rangeDate.each do |date|
         dateMask = date.split("-")
-
-        checkPasswdSize("#{word.chomp}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}")
-
+        puts "#{word}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}"
       end
   end
 end
 
 
-def wordFullDate(word) #
+def wordFullDate(word)
 
-  if $date == true
+  if $date == true && word.length + 8 >= $min && word.length + 8 <= $max
   
-    rangeDate = ((70.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 70 years 
+    rangeDate = ((60.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 60 years 
       rangeDate.each do |date|
         dateMask = date.split("-")
-        
-        checkPasswdSize("#{word.chomp}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}")
-
+        puts "#{word}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}"
       end
   end
 end
 
-def wordSpecialDate(word) # AQUI
+def wordSpecialDate(word)
 
-  if $date == true && $special == true 
+  if $date == true && $special == true && word.length + 7 >= $min
   
     (SPECIAL.length).times do |times|
-      rangeDate = ((70.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 70 years
+      rangeDate = ((60.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 60 years
+      rangeDate.each do |date|
+          dateMask = date.split("-")
+        if word.length + 9 <= $max
+          puts "#{word}#{SPECIAL[times]}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}"
+          puts "#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}#{SPECIAL[times]}#{word}"
+        end
+        if word.length + 7 <= $max
+          puts "#{word}#{SPECIAL[times]}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}"
+          puts "#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}#{SPECIAL[times]}#{word}"
+        end
+      end
+    end
+  end
+end
+
+def monthSpecialDate(word)
+
+  if $date == true && $special == true
+  
+    (SPECIAL.length).times do |times|
+      rangeDate = ((10.to_i.year.ago.to_date)..(Date.today)).to_a.map{|x| x.to_s(:db)} # 10 years
       rangeDate.each do |date|
   
         dateMask = date.split("-")        
-        checkPasswdSize("#{word.chomp}#{SPECIAL[times]}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}")
-        checkPasswdSize("#{dateMask[2]}#{dateMask[1]}#{dateMask[0][0..4]}#{SPECIAL[times]}#{word.chomp}")
-        checkPasswdSize("#{word.chomp}#{SPECIAL[times]}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}")
-        checkPasswdSize("#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}#{SPECIAL[times]}#{word.chomp}")
+        puts "#{word}#{SPECIAL[times]}#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}"
+        puts "#{dateMask[2]}#{dateMask[1]}#{dateMask[0][2..4]}#{SPECIAL[times]}#{word}"
         
       end
     end
   end
 end
+
 
 def twoLetters
   
   if $two == true
     ("aa".."zz").each do |letter|
       (1..999999).each do |num|
-        checkPasswdSize("#{letter.chomp}#{num.to_s.rjust(6, '0')}")
-        checkPasswdSize("#{num.to_s.rjust(6, '0')}#{letter.chomp}")
+        puts "#{letter}#{num.to_s.rjust(6, '0')}"
+        puts "#{num.to_s.rjust(6, '0')}#{letter}"
       end 
     end
   end
@@ -238,25 +249,25 @@ end
 
 def leetSpeak(word)
 
-  if $leet == true
+  if $leet == true && word.length >= $min && word.length <= $max
   
     originalWord = word.downcase.clone
     originalWord2 = word.downcase.clone
-
+    puts originalWord
+    
     REPLACEMENTS1.each {|rep| originalWord.gsub!(rep[0], rep[1])}
 
-        checkPasswdSize("#{originalWord.chomp}")
-        wordSpecial("#{originalWord.chomp}")
-        wordNum("#{originalWord.chomp}")
+        puts originalWord
+        wordSpecial(originalWord)
+        wordNum(originalWord)
 
     REPLACEMENTS2.each {|rep| originalWord2.gsub!(rep[0], rep[1])}
 
-        wordSpecial("#{originalWord2.chomp}")
-        checkPasswdSize("#{originalWord2.chomp}")
-        wordNum("#{originalWord2.chomp}")
+        puts originalWord2
+        wordSpecial(originalWord2)
+        wordNum(originalWord2)
 
   end
-
 end
 
 
@@ -273,25 +284,35 @@ end
 
 # Main
 
+if $months == true
+  MONTHS.each do |month| 
+    wordSpecialNum(month)
+    monthSpecialDate(month)
+  end
+end
+
 inputData.each_line do |line|
 
+  line.delete_suffix!("\n")
   leetSpeak(line)
 
   (3).times do |times|
-    if times == 0
-
-      checkPasswdSize("#{line.downcase!}")
+    if times == 0 
+      
       mangler(line)
 
-    elsif times == 1 && $capitalize == true
-
-      checkPasswdSize("#{line.capitalize!.chomp}")
-      mangler(line)
+    elsif times == 1 && $capitalize == true 
+      
+      if line.length >= $min && line.length <= $max
+        puts line.capitalize!
+      end
+      mangler(line.capitalize!)
 
     elsif times == 2 && $upcase == true
-
-      checkPasswdSize("#{line.upcase!.chomp}")
-      mangler(line)
+      if line.length >= $min && line.length <= $max
+        puts line.upcase!
+      end
+      mangler(line.upcase!)
 
     end
     
@@ -315,17 +336,17 @@ if $insane == true
       (3).times do |times|
       if times == 0
 
-        checkPasswdSize("#{line.downcase!}")
+        puts line.downcase!
         mangler(line)
 
       elsif times == 1 && $upcase == true
 
-        checkPasswdSize("#{line.upcase!}")
+        puts line.upcase!
         mangler(line)
 
       elsif times == 2 && $capitalize == true
 
-        checkPasswdSize("#{line.capitalize!}")
+        puts line.capitalize!
         mangler(line)
 
       end
